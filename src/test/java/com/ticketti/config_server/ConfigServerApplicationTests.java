fa0489e556a04bfe5a1952e5eaa618b5c3e7c907
marceloriv <ma.riveracontreras@duocuc.cl>
2026-04-19 21:23:39 -0400

    feat: :tada: primer commit

---
+++ src/test/java/com/ticketti/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketti.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
