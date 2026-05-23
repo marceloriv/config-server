4298124aaef77adcbcfa990ab3c794a2b31fea29
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-22 20:44:06 -0400

    fix(tests): :bug: Corregir importación de InjectMocks en ConfigServerServiceTest

--- src/test/java/com/ticketti/config_server/ConfigServerServiceTest.java
+++ src/test/java/com/ticketti/config_server/ConfigServerServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.anyString;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
